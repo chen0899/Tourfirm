d483a86caef964bb748bd7aa6f72bfe494405cbf
Illia Chenchak <chenchak0899@gmail.com>
2019-04-11 22:17:46 +0300

    fixModel

--- model/src/main/java/com/toufirm/Client.java
+++ model/src/main/java/com/toufirm/Client.java
@@ -1,34 +1,31 @@
 package com.toufirm;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 
 /**
  * Created by Illia Chenchak
  */
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
 public class Client {
 
-    @Getter @Setter
     private Long id;
 
-    @Getter @Setter
     private String firstName;
 
-    @Getter @Setter
     private String lastName;
 
-    @Getter @Setter
     private String email;
 
-    @Getter @Setter
     private String phone;
 
-    @Getter @Setter
     private Country country;
-
-    public Client() {
-    }
 
     public Client(Long id, String firstName, String lastName, String email, String phone, Country country) {
         this.id = id;
@@ -37,30 +34,5 @@ public class Client {
         this.email = email;
         this.phone = phone;
         this.country = country;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Client client = (Client) o;
-        return id.equals(client.id) &&
-                firstName.equals(client.firstName) &&
-                lastName.equals(client.lastName) &&
-                email.equals(client.email) &&
-                phone.equals(client.phone) &&
-                country.equals(client.country);
-    }
-
-    @Override
-    public String toString() {
-        return "com.toufirm.Client{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                ", phone='" + phone + '\'' +
-                ", country=" + country +
-                '}';
     }
 }
