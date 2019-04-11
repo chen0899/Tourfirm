060a1b1f3da4636cb43869991c5d0d0d4d4294d7
junior <pitbullboks@gmail.com>
2019-04-11 17:29:49 +0300

    added models Room and Hotel

--- model/src/main/java/com/toufirm/Hotel.java
+++ model/src/main/java/com/toufirm/Hotel.java
@@ -1,4 +1,23 @@
 package com.toufirm;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
 public class Hotel {
+    private Long id;
+    private String hotelName;
+    private String email;
+    private String phone;
+    private String city;
+    private String address;
+
 }
+
+
+
