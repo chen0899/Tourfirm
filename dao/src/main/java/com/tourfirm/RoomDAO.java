a20bedc7b11136661eeaf92597feb0adf53da213
junior <pitbullboks@gmail.com>
2019-04-14 16:42:49 +0300

    refactor interfaces RoomDAO and HotelDAO

--- dao/src/main/java/com/tourfirm/RoomDAO.java
+++ dao/src/main/java/com/tourfirm/RoomDAO.java
@@ -4,16 +4,13 @@ import com.toufirm.Room;
 
 import java.util.List;
 
-public interface RoomDAO  {
+public interface RoomDAO {
 
-    Room getRoomById(int id);
-
-    List<Room> findAll();
+    List<Room> findAllByHotelName(String hotelName);
 
     void save(Room room);
 
     void update(Room room);
 
     void delete(Room room);
-
 }
