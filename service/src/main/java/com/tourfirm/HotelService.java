d2428440255a23a5fef85a117205c0bbf9637f27
junior <pitbullboks@gmail.com>
2019-04-17 09:04:34 +0300

    added new intaerface HotelService

---
+++ service/src/main/java/com/tourfirm/HotelService.java
@@ -0,0 +1,18 @@
+package com.tourfirm;
+
+import com.toufirm.Hotel;
+
+import java.util.List;
+
+public interface HotelService {
+
+    Hotel findHotelByName(String hotelName);
+
+    List<Hotel> findAllByCity(String cityName);
+
+    void save(Hotel hotel);
+
+    void update(Hotel hotel);
+
+    void delete(Hotel hotel);
+}
