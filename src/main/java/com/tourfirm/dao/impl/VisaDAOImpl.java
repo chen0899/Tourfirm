7b7b60ee59537eb43d1aa20cda3ef180cde19d95
Anastasia Hanuliak <anastasia.hanuliak@gmail.com>
2019-04-18 23:12:30 +0300

    add Visa and City DAO

---
+++ src/main/java/com/tourfirm/dao/impl/VisaDAOImpl.java
@@ -0,0 +1,14 @@
+package com.tourfirm.dao.impl;
+
+import com.tourfirm.dao.VisaDAO;
+import com.tourfirm.entity.Visa;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+public class VisaDAOImpl extends AbstractDAO<Visa, Integer> implements VisaDAO {
+    protected VisaDAOImpl(Class<Visa> entityClass) {
+        super(entityClass);
+    }
+}
