cb6fe97d364812c0bc7e5511e8eb2e93047cf198
Illia Chenchak <chenchak0899@gmail.com>
2019-04-24 12:31:10 +0300

    addFindVisaByCountry

--- src/main/java/com/tourfirm/controller/TaskController.java
+++ src/main/java/com/tourfirm/controller/TaskController.java
@@ -41,56 +41,50 @@ public class TaskController {
     private VisaService visaService;
 
     @GetMapping("/task")
-    public String allTasks(){
+    public String allTasks() {
         return "tasks";
     }
 
     @GetMapping("/task1")
-    public String citiesAndCountries(Model model){
-        model.addAttribute("countryList",countryService.findAll());
+    public String citiesAndCountries(Model model) {
+        model.addAttribute("countryList", countryService.findAll());
         model.addAttribute("cityList", cityService.findAll());
         return "task1";
     }
 
 
     @GetMapping("/task2")
-    public String hotelsByCityName(){
+    public String hotelsByCityName() {
         return "task2";
     }
 
     @GetMapping("/task2-out")
-    public String outHotels(@RequestParam String cityName, Model model){
-        model.addAttribute("hotelList",hotelService.findAllByCity(cityName));
+    public String outHotels(@RequestParam String cityName, Model model) {
+        model.addAttribute("hotelList", hotelService.findAllByCity(cityName));
         return "task2-out";
     }
 
     @GetMapping("/task3")
-    public String visaByClientName(){
+    public String visaByClientName() {
         return "task3";
     }
 
     @GetMapping("/task3-out")
-    public String outVisa(@RequestParam String lastName, Model model){
+    public String outVisa(@RequestParam String lastName, Model model) {
         model.addAttribute("visaList", visaService.findAllVisaByClient(lastName));
         return "task3-out";
     }
-    @PostMapping("/availeble-room")
-    public String availebleRoom(@RequestParam Date startDate, @RequestParam Date endDate,
-                                @RequestParam String clientId, @RequestParam String hotelId, Model model){
 
-        List<Room> available = null;
-        List<Booking> bookings = bookingService.findAll();
-        List<Long> listId = new ArrayList<>();
-        List<Room> rooms = roomService.findAllByHotelId(Long.parseLong(hotelId));
-        for (Booking booking: bookings
-             ) {
-
-            listId.add(Long.valueOf(booking.getRoom().getId()));
-
-        }
+    @GetMapping("/task4")
+    public String visaByCountryName() {
+        return "task4";
+    }
 
 
+    @GetMapping("/task4-out")
+    public String outVisaByCountry(@RequestParam String countryName, Model model) {
+        model.addAttribute("visaList", visaService.findAllVisaByCountry(countryName));
+        return "task4-out";
 
-        return "book-room";
     }
 }
---
+++ src/main/webapp/WEB-INF/views/task4.jsp
@@ -0,0 +1,28 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<html>
+<body>
+
+<div class="form-group mt-3 ">
+    <div class="form-row">
+        <div class="form-group" style="width: 80%; margin-left: 2%">
+            <form method="get" action="/task4-out">
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Enter country name</label>
+                    <input type="text" class="form-control col-sm-4" name="countryName"/>
+                </div>
+                <div class="form-group row text-right">
+                    <button class="btn btn-outline-info col-sm-4" style="margin-left: 16.7%" type="submit"
+                            name="update">
+                        Enter
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+</body>
+</html>
